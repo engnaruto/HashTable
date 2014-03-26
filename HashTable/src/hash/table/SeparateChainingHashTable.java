bc36948cfb635eeae791b3473758514148719956
Naruto <Naruto>
2014-03-26 18:23:33 +0200

    start

---
+++ HashTable/src/hash/table/SeparateChainingHashTable.java
@@ -0,0 +1,44 @@
+package hash.table;
+
+public class SeparateChainingHashTable<K, V> implements HashTable<K, V> {
+
+	@Override
+	public void put(K key, V value) {
+
+	}
+
+	@Override
+	public String get(K key) {
+
+		return null;
+	}
+
+	@Override
+	public void delete(K key) {
+
+	}
+
+	@Override
+	public boolean contains(K key) {
+
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+
+		return false;
+	}
+
+	@Override
+	public int size() {
+
+		return 0;
+	}
+
+	@Override
+	public Iterable<K> keys() {
+		return null;
+	}
+
+}
